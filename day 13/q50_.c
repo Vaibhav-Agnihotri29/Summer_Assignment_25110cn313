58f3ec2f0e420ce7ed0a9b31a91496fa2813b810
Vaibhav-Agnihotri29 <maxvaibhav122@gmail.com>
2026-06-15 09:13:24 +0530

    Add files via upload

---
+++ day 13/q50_.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+
+    int a[55], n, i, sum = 0;
+    float avg;
+
+scanf("%d", &n);
+
+ for (i = 0; i < n; i++) {
+
+scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    avg = (float)sum / n;
+ printf("Sum = %d\n", sum);
+    printf("Avg = %.2f", avg);
+
+return 0;
+}
